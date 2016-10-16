dbe0b7e813088a05d4509e698701bfc38e84667a
JoaoChaves96 <joaovitor1906@hotmail.com>
2016-10-16 02:03:58 +0100

    some refactoring

--- reader/XMLscene.js
+++ reader/XMLscene.js
@@ -1,3 +1,4 @@
+
 function XMLscene() {
     CGFscene.call(this);
 }
@@ -12,6 +13,8 @@ XMLscene.prototype.init = function (application) {
 
     this.initLights();
 
+    this.quad = new MyUnitCubeQuad(this);
+
     this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     this.gl.clearDepth(100.0);
@@ -23,9 +26,12 @@ XMLscene.prototype.init = function (application) {
 };
 
 XMLscene.prototype.initLights = function () {
-
-	this.lights[0].setPosition(2, 3, 3, 1);
-    this.lights[0].setDiffuse(1.0,1.0,1.0,1.0);
+    this.lights[0].setPosition(2, 3, 3, 1);
+    this.lights[0].setAmbient(0, 0, 0, 1);
+    this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.lights[0].setSpecular(1.0, 1.0, 1.0, 1.0);
+    this.lights[0].setVisible(true);
+    this.lights[0].enable();
     this.lights[0].update();
 };
 
@@ -45,13 +51,38 @@ XMLscene.prototype.setDefaultAppearance = function () {
 XMLscene.prototype.onGraphLoaded = function ()
 {
 	this.gl.clearColor(this.graph.background[0],this.graph.background[1],this.graph.background[2],this.graph.background[3]);
-	this.lights[0].setVisible(true);
-    this.lights[0].enable();
+  this.lights[0].setVisible(true);
+  this.lights[0].enable();
+};
+
+XMLscene.prototype.loadLights = function(){
+  for(var i = 0; i < this.graph.lights.length; i++){
+    this.lights[i].setPosition(this.graph.lights[i].location[0],this.graph.lights[i].location[1],this.graph.lights[i].location[2],this.graph.lights[i].location[3]);
+    this.lights[i].setAmbient(this.graph.lights[i].ambient[0],this.graph.lights[i].ambient[1],this.graph.lights[i].ambient[2],this.graph.lights[i].ambient[3]);
+    this.lights[i].setDiffuse(this.graph.lights[i].diffuse[0],this.graph.lights[i].diffuse[1],this.graph.lights[i].diffuse[2],this.graph.lights[i].diffuse[3]);
+    this.lights[i].setSpecular(this.graph.lights[i].specular[0],this.graph.lights[i].specular[1],this.graph.lights[i].specular[2],this.graph.lights[i].specular[3]);
+    this.lights[i].update();
+  }
+};
+
+XMLscene.prototype.updateLights = function(){
+  for(var i = 0; i < this.lights.length; i++){
+    this.lights[i].setVisible(true);
+    if (this.graph.lights[i].enabled)
+      this.lights[i].enable();
+    else {
+      this.lights[i].disable();
+    }
+  }
+
+  for (var i = 0; i < this.lights.length; i++)
+    this.lights[i].update();
+
 };
 
 XMLscene.prototype.display = function () {
 	// ---- BEGIN Background, camera and axis setup
-
+this.onGraphLoaded();
 	// Clear image and depth buffer everytime we update the scene
     this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
@@ -66,7 +97,7 @@ XMLscene.prototype.display = function () {
 	// Draw axis
 	this.axis.display();
 
-
+  this.quad.display();
 
 	this.setDefaultAppearance();
 
