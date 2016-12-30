6112dcef7811df2a6c7dc438003233ff277352e5
Inês Teixeira <inescfteixeira@gmail.com>
2016-12-30 21:52:58 +0000

    another scenario added

--- proj3/reader/Scenery.js
+++ proj3/reader/Scenery.js
@@ -22,12 +22,29 @@ function Scenery(scene) {
 	this.materialC = new CGFappearance(this.scene);
 	this.materialC.loadTexture("./resources/images/satelite2.jpg");
 
+	this.materialFloor = new CGFappearance(this.scene);
+	this.materialFloor.loadTexture("./resources/images/floorR.jpg");
+
+	this.materialWall = new CGFappearance(this.scene);
+	this.materialWall.loadTexture("./resources/images/wallR.jpg");
+
+	this.materialTable = new CGFappearance(this.scene);
+	this.materialTable.loadTexture("./resources/images/wood.jpg");
+
+
 	this.initSpace();
+	this.initNormalRoom();
 
 };
 
 Scenery.prototype.display = function(){
 
+	//this.spaceDisplay();
+	this.normalRoomDisplay();
+
+}
+
+Scenery.prototype.spaceDisplay = function(){
 	this.scene.pushMatrix();
 	this.scene.multMatrix(this.spaceWallsMatrix);
 	//this.scene.universeMat.apply();
@@ -109,7 +126,6 @@ Scenery.prototype.display = function(){
 	this.materialC.apply();
 	this.board2.display();
 	this.scene.popMatrix();
-
 }
 
 Scenery.prototype.initSpace = function(){
@@ -206,4 +222,111 @@ Scenery.prototype.initSpaceMatrixes = function(){
 	mat4.translate(this.board2Matrix, this.board2Matrix, [33,0,7.5]);
 	mat4.scale(this.board2Matrix, this.board2Matrix, [16, 0.2, 8]);
 
+}
+
+Scenery.prototype.normalRoomDisplay = function(){
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.roomFMatrix);
+	this.materialFloor.apply();
+	this.roomF.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.roomW1Matrix);
+	this.materialWall.apply();
+	this.roomW.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.roomW2Matrix);
+	this.materialWall.apply();
+	this.roomW.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.tableMatrix);
+	this.materialTable.apply();
+	this.table.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.legTableMatrix1);
+	this.materialTable.apply();
+	this.legTable.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.legTableMatrix2);
+	this.materialTable.apply();
+	this.legTable.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.legTableMatrix3);
+	this.materialTable.apply();
+	this.legTable.display();
+	this.scene.popMatrix();
+
+	this.scene.pushMatrix();
+	this.scene.multMatrix(this.legTableMatrix4);
+	this.materialTable.apply();
+	this.legTable.display();
+	this.scene.popMatrix();
+
+
+}
+
+Scenery.prototype.initNormalRoom = function(){
+
+	this.roomF=	new MyUnitCubeQuad(this.scene);
+	this.roomW = new MyUnitCubeQuad(this.scene);
+
+	this.table = new MyUnitCubeQuad(this.scene);
+	this.legTable = new MyUnitCubeQuad(this.scene);
+
+	this.initNormalRoomMatrixes();
+
+}
+
+Scenery.prototype.initNormalRoomMatrixes = function(){
+
+	this.roomFMatrix = mat4.create();
+	mat4.identity(this.roomFMatrix);
+	mat4.translate(this.roomFMatrix, this.roomFMatrix, [10, -7,10]);
+	mat4.scale(this.roomFMatrix, this.roomFMatrix, [70, 3, 50]);
+
+	this.roomW1Matrix = mat4.create();
+	mat4.identity(this.roomW1Matrix);
+	mat4.translate(this.roomW1Matrix, this.roomW1Matrix, [-40, 18,10]);
+	mat4.scale(this.roomW1Matrix, this.roomW1Matrix, [50, 50, 50]);
+
+	this.roomW2Matrix = mat4.create();
+	mat4.identity(this.roomW2Matrix);
+	mat4.translate(this.roomW2Matrix, this.roomW2Matrix, [0, 18,-40]);
+	mat4.scale(this.roomW2Matrix, this.roomW2Matrix, [70, 50, 50]);
+
+	this.tableMatrix = mat4.create();
+	mat4.identity(this.tableMatrix);
+	mat4.translate(this.tableMatrix, this.tableMatrix, [12, -0.4, 7]);
+	mat4.scale(this.tableMatrix, this.tableMatrix, [20, 0.5, 12]);
+
+	this.legTableMatrix1 = mat4.create();
+	mat4.identity(this.legTableMatrix1);
+	mat4.translate(this.legTableMatrix1, this.legTableMatrix1, [3, -3, 12]);
+	mat4.scale(this.legTableMatrix1, this.legTableMatrix1, [0.5, 5, 0.5]);
+
+	this.legTableMatrix2 = mat4.create();
+	mat4.identity(this.legTableMatrix2);
+	mat4.translate(this.legTableMatrix2, this.legTableMatrix2, [21, -3, 12]);
+	mat4.scale(this.legTableMatrix2, this.legTableMatrix2, [0.5, 5, 0.5]);
+
+	this.legTableMatrix3 = mat4.create();
+	mat4.identity(this.legTableMatrix3);
+	mat4.translate(this.legTableMatrix3, this.legTableMatrix3, [21, -3, 2]);
+	mat4.scale(this.legTableMatrix3, this.legTableMatrix3, [0.5, 5, 0.5]);
+
+	this.legTableMatrix4 = mat4.create();
+	mat4.identity(this.legTableMatrix4);
+	mat4.translate(this.legTableMatrix4, this.legTableMatrix4, [3, -3, 2]);
+	mat4.scale(this.legTableMatrix4, this.legTableMatrix4, [0.5, 5, 0.5]);
 }
